44ffa566e618f7030af9c3ad9b0cb7bc312a602a
Keith Jackson Jr <kjacksonjr87@gmail.com>
2018-05-31 16:43:45 -0400

    made some updates

--- TrelloReporter.php
+++ TrelloReporter.php
@@ -13,12 +13,12 @@
 		return json_decode($results);
   	}
 
-	$options = getopt("b:t:",['overview::','detailed::']);
+	$options = getopt("b:t:",['overview::','detailed::','nameonly::','list::']);
 
 	$key = getenv('TRELLO_API_KEY',true);
   	$token = getenv('TRELLO_API_TOKEN',true);
 
-  	if( !isset($options['b']) || !isset($options['t']) ) {
+  	if( !isset($options['b']) ) {
   		print 'Invalid Usage: Please make sure board id and tag are set!'.PHP_EOL;
   		exit(0);
   	}
@@ -29,10 +29,6 @@
   	}
 
   	$board = $options['b'];
-  	$tag = $options['t'];
-  	$listArray = [];
-	$cardsIn = [];
-	$listCounts = [];
 
 	$lists = get('list',$board,$key,$token);
 	foreach($lists as $l) {
@@ -40,33 +36,55 @@
 	}
 
 	$cards = get('cards',$board,$key,$token);
-	foreach($cards as $c) {
-		foreach($c->labels as $l){
-			if (strpos($l->name,$tag) !== false) {
-				$cardsIn[] = [
-					'id' => $c->id,
-					'name' => $c->name,
-					'list' => $c->idList
-				];
 
-				if (!isset($listCounts[$c->idList])) {
-					$listCounts[$c->idList] = 1;
-				} else {
-					$listCounts[$c->idList]++;
-				}
-			}	
+	if (isset($options['list'])) {
+		print "There are currently " . count($cards) . " cards for this board.".PHP_EOL;
+
+		foreach($cards as $c) {
+			print "{$c->name}".PHP_EOL;
 		}
-	}
+	} else if (isset($options['t'])){
 
-	print "{$tag} currently has " . count($cardsIn) . " cards tagged.".PHP_EOL;
+var_dump($options);exit;
 
-	if(isset($options['overview'])) {
-		foreach($listCounts as $id => $total) {
-			print "{$total} item(s) in '{$listArray[$id]}'".PHP_EOL;
+		$tag = $options['t'];
+	  	$listArray = [];
+		$cardsIn = [];
+		$listCounts = [];
+
+		foreach($cards as $c) {
+			foreach($c->labels as $l){
+				if (strpos($l->name,$tag) !== false) {
+					$cardsIn[] = [
+						'id' => $c->id,
+						'name' => $c->name,
+						'list' => $c->idList
+					];
+
+					if (!isset($listCounts[$c->idList])) {
+						$listCounts[$c->idList] = 1;
+					} else {
+						$listCounts[$c->idList]++;
+					}
+				}	
+			}
 		}
-	} else if (isset($options['detailed'])) {
-		foreach($cardsIn as $c){
-			print "{$c['name']} is in {$listArray[$c['list']]}".PHP_EOL;
+
+		if(isset($options['t']) && $options['t']) {
+			print "{$tag} currently has " . count($cardsIn) . " cards tagged.".PHP_EOL;
+
+		} else if(isset($options['nameonly'])) {
+			foreach($cardsIn as $c){
+				print "{$c['name']}".PHP_EOL;
+			}
+		} else if(isset($options['overview'])) {
+			foreach($listCounts as $id => $total) {
+				print "{$total} item(s) in '{$listArray[$id]}'".PHP_EOL;
+			}
+		} else if (isset($options['detailed'])) {
+			foreach($cardsIn as $c){
+				print "{$c['name']} is in {$listArray[$c['list']]}".PHP_EOL;
+			}
 		}
 	}
 
